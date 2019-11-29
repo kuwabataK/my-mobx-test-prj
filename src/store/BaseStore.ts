34dea349211f142447b37b58cd6d81ccbe39fc49
kuwabara soma <kuwabara.soma@run-edge.com>
2019-11-29 09:09:41 +0900

    返り値の型をundefinedから never型に変更

--- src/store/BaseStore.ts
+++ src/store/BaseStore.ts
@@ -15,34 +15,37 @@ export abstract class BaseStore {
    * このストアに定義されたメソッドの一覧を返します。
    * ここで取得したメソッドはコンポーネントの中で、関数にラップすることなく呼び出せるようになります。
    * 例えば以下のように使えます
-   * 
+   *
    * <pre><code>
    * const { increment } = store.counterStore.mapActions()
    * return <button onClock={increment}></div>
    * </code></pre>
-   * 
+   *
    * mapActionsを使わずに展開した関数はthisの参照が壊れてしまうので、うまく動かない場合があります。
    * 例えば以下のようなコードはうまく動きません
-   * 
+   *
    * <pre><code>
    * const { increment } = store.counterStore
    * return <button onClock={increment}></div>
    * </code></pre>
-   * 
+   *
    * また、メソッド以外のメンバは取得できません、すべてundefinedになります
-   * 
+   *
    * @return object メソッドのみを抽出したMap
-   * 
+   *
    */
-  mapActions(){
-    let res: Partial<{[K in keyof this]: this[K] extends Function ? this[K] : undefined}> = {}
-    Object.getOwnPropertyNames((this as any)['__proto__']).forEach((key) => {
-      if (typeof (this as any)[key] === 'function'){
-        (res as any)[key] = (...args: any) => (this as any)[key](...args)
+  mapActions() {
+    let res: Partial<
+      { [K in keyof this]: this[K] extends Function ? this[K] : never }
+    > = {};
+    Object.getOwnPropertyNames((this as any)["__proto__"]).forEach(key => {
+      if (typeof (this as any)[key] === "function") {
+        (res as any)[key] = (...args: any) => (this as any)[key](...args);
       }
-    })
-    return res as Omit<{[K in keyof this]: this[K] extends Function ? this[K] : undefined}, 'mapActions'>
+    });
+    return res as Omit<
+      { [K in keyof this]: this[K] extends Function ? this[K] : never },
+      "mapActions"
+    >;
   }
-
-
 }
